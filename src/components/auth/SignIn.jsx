31d1e31cd936ba02ae4caab0474c30965a0315e0
Wolfy4H <Leon.Kersten@outlook.com>
2023-06-20 13:18:54 +0200

    log in terms of privacy

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -6,61 +6,69 @@ const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPrivacyPopup, setShowPrivacyPopup] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
 
   const signIn = (e) => {
     e.preventDefault();
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         console.log(userCredential);
+        setShowPrivacyPopup(true);
       })
       .catch((error) => {
         console.log(error);
       });
-    setShowPrivacyPopup(true); // Show the privacy popup after sign in attempt
   };
 
   const handlePrivacyAgreement = (agreed) => {
     if (agreed) {
-      // Create account and log in
-      // ...
-      // Code to create account and log in goes here
+      // Perform any additional logic or actions before logging in
+      setLoggedIn(true); // Simulating successful login
     } else {
-      // Reset the form
       setEmail("");
       setPassword("");
     }
-    setShowPrivacyPopup(false); // Hide the privacy popup
+    setShowPrivacyPopup(false);
   };
 
   return (
     <div className="sign-in-container">
-      <form onSubmit={signIn}>
-        <h1>LOG IN</h1>
-        <input
-          type="text"
-          placeholder="enter your email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="enter your password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Log in</button>
-      </form>
+      {!loggedIn && (
+        <form onSubmit={signIn}>
+          <h1>LOG IN</h1>
+          <input
+            type="text"
+            placeholder="enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Log in</button>
+        </form>
+      )}
 
       {showPrivacyPopup && (
         <div className="privacy-popup">
           <h2>Terms of Privacy</h2>
-          {/* Privacy terms content goes here */}
+          As user who is signing this contract agree with whatever I post will
+          be on here until I delete it, I agree to the fact that eveything I say
+          or do on this can be seen by other users. Further more do I agree that
+          birds are fricking awesome and that I'll always treat them right.
+          Lastly I agree to the fact that Iam resposible for my own behavior on
+          this app and will take consecuenses if nessesary.
           <button onClick={() => handlePrivacyAgreement(true)}>Agree</button>
           <button onClick={() => handlePrivacyAgreement(false)}>
             Disagree
           </button>
         </div>
       )}
+
+      {loggedIn && <p>You have successfully logged in.</p>}
     </div>
   );
 };
